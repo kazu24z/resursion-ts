319ef2fadf3c3130a2a2ed9bb49c8e75174f22a7
kazu24z <progra.taro2021@gmail.com>
2025-01-19 21:39:13 +0900

    No.490_挿入ソート_レビューコード

--- src/problems/no-490.ts
+++ src/problems/no-490.ts
@@ -1,44 +1,18 @@
 // 実装する関数
 export function main(arr: number[]): number[] {
-  // 挿入ソートの実装
-  
-  // 2番目以降のindexに対する処理（iは処理対象のindex）
-  for(let i=1; i<arr.length; i++) {
-    let sortIndex = i;
-    // 整列済み部分に対する比較処理（jはソート済みのindex）
-    for(let j=0; j<i; j++){
-      if(arr[i] < arr[j]) {
-        // もし処理対象がソート済み値より小さい場合、その時点のソート済み値を持つindexが挿入箇所になる
-        sortIndex = j;
-        // 見つかったら以降の処理は不要
-        break;
-      }
+  // コードレビュー版
+  for (let i = 1; i < arr.length; i++) {
+    const current = arr[i];
+    let j = i - 1; // jはソート済み部分の最後のindex
+    // ソート済み部分を逆順にたどり、挿入位置を見つける （ケツから辿ると、ずらし処理も合わせてできる）
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
     }
-    // その回の結果を受けてまとめ処理をする
-    // ソート済みはi-1のindexまで
-    // 今回処理対象の値
-    const temp = arr[i];
-
-    // sortIndex + 1から iまでの値を1つずつずらす
-    for(let k=i; k>sortIndex; k--) {
-      arr[k] = arr[k-1];
-    }
-
-    arr[sortIndex] = temp; // その回の値をsortIndexの場所に入れる
+    arr[j + 1] = current;
   }
-
   return arr;
-
 }
-
-// メモ
-/**
- * 整列済みの配列のどのインデックス番号に入るのか
- * 
- * 整列済みの要素の中から、自分よりでかい値が出てきた時点で、そのindexが挿入先のindexになる
- * →そこに新しいのを入れて、以降を後ろにずらす処理が必要
- * これは、次回分に影響がない
- */
 
 // 入力データ（問題ごとに修正）
 const data = [
